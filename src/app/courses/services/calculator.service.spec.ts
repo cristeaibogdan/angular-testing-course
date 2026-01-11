e0c49940f8276bb1acf8700494c6e52dac93f9cc
Bogdan <cristea.ibogdan@gmail.com>
2026-01-11 14:40:05 +0200

    End of section 1

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,38 @@
+import { CalculatorService } from "./calculator.service";
+import { TestBed } from "@angular/core/testing";
+import { LoggerService } from "./logger.service";
+
+// Replace `describe` with `xdescribe` to disable the whole test suite
+// To focus on a specific test suite, replace `describe` with `fdescribe`
+describe("CalculatorService", () => {
+  let underTest: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    underTest = TestBed.inject(CalculatorService);
+  });
+
+  // Replace `it` with `xit` to disable the test
+  // To focus on a specific test, replace `it` with `fit`
+  it("should add two numbers", () => {
+    const actual = underTest.add(2, 2);
+
+    expect(actual).toBe(4, "unexpected add result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+
+  it("should subtract two numbers", () => {
+    const actual = underTest.subtract(2, 2);
+
+    expect(actual).toBe(0, "unexpected subtraction result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+});
